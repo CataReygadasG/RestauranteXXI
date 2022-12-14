f0c73adcf22892c5494dbfcf8182ca7dd0b74ab0
CataReygadasG <catafranrey@gmail.com>
2022-12-14 19:13:46 -0300

    modificaciones

--- src/fb.js
+++ src/fb.js
@@ -1,17 +1,4 @@
-/*import firebase from "firebase/compat/app"
-import "firebase/compat/auth"
-import { getFirestore } from 'firebase/firestore';
-//bd de firebase(firestore)
-export const app = firebase.initializeApp({
-    "projectId": process.env.REACT_APP_FIREBASE_PROJECTID,
-    "storageBucket": process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-    "locationId": process.env.REACT_APP_FIREBASE_LOCATIONID,
-    "apiKey": process.env.REACT_APP_FIREBASE_APIKEY,
-    "authDomain": process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-    "messagingSenderId": process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID
-  });
 
- export const db = getFirestore();*/
 
  // Importamos la función para inicializar la aplicación de Firebase
 import { initializeApp } from "firebase/app";
